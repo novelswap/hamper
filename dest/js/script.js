446c892ceb33689301059183be9a63c529677b79
Josh Ghent <me@joshghent.com>
2017-03-15 01:42:33 +0000

    Configured basic gulp tasks.

---
+++ dest/js/script.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+const test = () => {
+  console.log("hello");
+};
--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,32 @@ var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var babel = require('babelify');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./dest/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dest/css/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./dest/sass/*.scss', ['sass']);
+});
 
 function compile(watch) {
-  var bundler = watchify(browserify('./src/js/*.js', { debug: true }).transform(babel));
+  var bundler = watchify(browserify('./dest/js/script.js', { debug: true, exclude: './dest/js/bundle.js' }).transform(babel.configure({
+    presets: ['es2015']
+  })));
 
   function rebundle() {
     bundler.bundle()
       .on('error', function(err) { console.error(err); this.emit('end'); })
-      .pipe(source('build.js'))
+      .pipe(source('bundle.js'))
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./dest'));
+      .pipe(gulp.dest('./dest/js'));
   }
 
   if (watch) {
@@ -33,7 +47,14 @@ function watch() {
   return compile(true);
 };
 
-gulp.task('build', function() { return compile(); });
-gulp.task('watch', function() { return watch(); });
+gulp.task('build', ['sass'], function() { 
+  compile();
+});
+
+gulp.task('watch', function() { 
+  watch(); 
+  gulp.watch('./dest/sass/*.scss', ['sass']);
+  gutil.log(gutil.colors.bgGreen('Watching for changes...'));
+});
 
 gulp.task('default', ['watch']);

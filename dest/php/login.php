0bb18e98fa78460f756e768936937e63f1d76ca0
Josh Ghent <me@joshghent.com>
2017-03-17 01:00:29 +0000

    Changed login process.

--- dest/php/login.php
+++ dest/php/login.php
@@ -1,6 +1,9 @@
 <?php
 require('./../../includes/dbConnect.inc.php');
 include('./../../includes/config.php');
+require('./../../vendor/autoload.php');
+
+$db = new MysqliDb($mysqli);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -15,8 +18,23 @@ else if (empty($password)) {
   $response['status'] = 'error';
   $response['message'] = 'Please enter your password.';
 } else {
-  $response['status'] = 'success';
-  $response['message'] = 'Successfully signed in.';
+  $result = $db->where('email', "$email")->where('active', 1)->where('deleted', 0)->get('users', null, "userID, password");
+
+  foreach($result as $row) {
+    if (password_verify($password, $row['password'])) {
+      $db->where('userID', $row['userID'])->update('users', array(
+        'lastLogin' => date('Y-m-d H:i:s')
+      ));
+
+      session_start();
+      $_SESSION['userID'] = $row['userID'];
+
+      $response['status'] = 'success';
+    } else {
+      $response['status'] = 'error';
+      $response['message'] = 'Your password is incorrect.';
+    }
+  }
 }
 
 echo json_encode($response);
